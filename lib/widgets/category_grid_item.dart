74a1ab83d74343a7b8fec046c3ada77695f00b41
adogbeji <baka_1993@yahoo.co.uk>
2024-06-07 13:36:34 +0100

    Add category data & starting code for CategoryGridItem widget

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Category Grid Item'),
+      ),
+    );
+  }
+}
