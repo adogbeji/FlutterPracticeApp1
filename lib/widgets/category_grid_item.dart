d587d028de04b2aebfca9b5948d385536e2120ae
adogbeji <baka_1993@yahoo.co.uk>
2024-06-09 17:50:45 +0100

    Start building CategoryGridItem widget

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:practice_app_1/models/category.dart';
+
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key});
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Category Grid Item'),
-      ),
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
+        color: Theme.of(context).colorScheme.onBackground,
+      ),),
     );
   }
 }
