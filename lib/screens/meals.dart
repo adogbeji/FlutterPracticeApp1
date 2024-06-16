968bfaa5c7301fe739cb6de34898ce3c33cac246
adogbeji <baka_1993@yahoo.co.uk>
2024-06-16 17:50:31 +0100

    Add new data to dummy_data.dart & start building meals screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:practice_app_1/models/meal.dart';
+
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Meals Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
       ),
     );
   }
